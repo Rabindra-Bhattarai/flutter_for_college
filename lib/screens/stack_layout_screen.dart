3f6bd19acd525d01e50c08b3cd06bbe97bb61a51
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2025-12-09 23:24:11 +0545

    stack layout upto slide no 5

--- lib/screens/stack_layout_screen.dart
+++ lib/screens/stack_layout_screen.dart
@@ -6,12 +6,53 @@ class StackLayoutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Stack Layout'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
-      body: const Center(
-        child: Text('stack layout', style: TextStyle(fontSize: 24)),
+
+      body: SafeArea(
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              height: 400,
+              width: 400,
+              color: Colors.amber,
+              child: const Text('I am First Container'),
+            ),
+            Positioned(
+                top: 1,
+                right: 1,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                  child: const Text('I am the second Container'),
+                )
+            ),
+            //Class Work 1
+            //
+            // Positioned(
+            //     bottom:0,
+            //     left: 0,
+            //     child: Container(
+            //     height: 100,
+            //     width:100,
+            //     color: Colors.red,
+            //     child: const Text('Hello I am third container'),
+            // ))
+
+
+
+            Positioned(
+                bottom: -20,
+                right:400/2-50,
+                child: Container(
+                  height: 100,
+                  width:100,
+                  color: Colors.red,
+                  child: const Text('Hello I am third container'),
+            ))
+
+          ],
+        ),
       ),
     );
   }
